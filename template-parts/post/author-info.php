b076204660abad7d271746a024659c3c4609ac85
NATIVE INSIDE <40865567+nativeinside@users.noreply.github.com>
2018-10-16 20:54:41 +0600

    Add file header

--- template-parts/post/author-info.php
+++ template-parts/post/author-info.php
@@ -1,9 +1,19 @@
+<?php
+/**
+ * Displays author information
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0
+ */
+?>
+
 <?php if ( (bool) get_the_author_meta( 'description' ) ) : ?>
 <div class="author-description">
 	<h2 class="author-title">
 		<span class="author-heading"><?php _e( 'Published by', 'twentynineteen' ); ?></span>
 		<?php echo get_the_author(); ?>
-	</h2>
+	</h2><!-- .author-title -->
 	<p class="author-bio">
 		<?php the_author_meta( 'description' ); ?>
 		<a class="author-link" href="<?php echo esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ); ?>" rel="author">
