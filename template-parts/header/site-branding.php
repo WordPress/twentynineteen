309feacb4ccf30192913c5cb38ac47fccb9504cf
NATIVE INSIDE <40865567+nativeinside@users.noreply.github.com>
2018-10-16 20:50:38 +0600

    Add file header

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -1,3 +1,14 @@
+<?php
+/**
+ * Displays header site branding
+ *
+ * @package WordPress
+ * @subpackage Twenty_Nineteen
+ * @since 1.0
+ */
+
+?>
+
 <div class="site-branding">
 
 	<?php if ( has_custom_logo() ) : ?>
