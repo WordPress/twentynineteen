be9ec8362e54a0a85bf7bc251c8c8d550380b60b
fabiankaegy <fabian@arvernus.info>
2018-10-16 22:53:44 +0200

    Added two widgetareas before the siteinfo

--- footer.php
+++ footer.php
@@ -15,6 +15,7 @@
 	</div><!-- #content -->
 
 	<footer id="colophon" class="site-footer">
+		<?php get_template_part( 'template-parts/footer/footer', 'widgets' ); ?>
 		<div class="site-info">
 			<a class="site-name" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>,
 			<?php
---
+++ template-parts/footer/footer-widgets.php
@@ -0,0 +1,20 @@
+<?php
+if ( is_active_sidebar( 'sidebar-1' ) ||
+	 is_active_sidebar( 'sidebar-2' ) ) :
+?>
+
+	<aside class="widget-area" role="complementary" aria-label="<?php esc_attr_e( 'Footer', 'twentynineteen' ); ?>">
+		<?php
+		if ( is_active_sidebar( 'sidebar-1' ) ) { ?>
+			<div class="widget-column footer-widget-1">
+				<?php dynamic_sidebar( 'sidebar-1' ); ?>
+			</div>
+		<?php }
+		if ( is_active_sidebar( 'sidebar-1' ) ) { ?>
+			<div class="widget-column footer-widget-2">
+				<?php dynamic_sidebar( 'sidebar-2' ); ?>
+			</div>
+		<?php } ?>
+	</aside><!-- .widget-area -->
+
+<?php endif; ?>
