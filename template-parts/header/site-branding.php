93a32b07c0ab058ee89146bf6ab0c2cab816320a
audrasjb <[audrasjb@gmail.com]>
2018-10-16 22:40:31 +0200

    fix a small wpcs issue with nav menus

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -20,21 +20,25 @@
 	</p>
 	<?php if ( has_nav_menu( 'menu-1' ) ) : ?>
 		<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'twentynineteen' ); ?>">
-			<?php wp_nav_menu( array(
-			'theme_location' => 'menu-1',
-			'menu_class'     => 'main-menu',
-		) ); ?>
+			<?php 
+			wp_nav_menu( array(
+				'theme_location' => 'menu-1',
+				'menu_class'     => 'main-menu',
+			) ); 
+			?>
 		</nav><!-- #site-navigation -->
 	<?php endif; ?>
 	<?php if ( has_nav_menu( 'social' ) ) : ?>
 		<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentynineteen' ); ?>">
-			<?php wp_nav_menu( array(
+			<?php 
+			wp_nav_menu( array(
 				'theme_location' => 'social',
 				'menu_class'     => 'social-links-menu',
 				'link_before'    => '<span class="screen-reader-text">',
 				'link_after'     => '</span>' . twentynineteen_get_icon_svg( 'link' ),
 				'depth'          => 1,
-			) ); ?>
+			) ); 
+			?>
 		</nav><!-- .social-navigation -->
 	<?php endif; ?>
 </div><!-- .site-branding -->
