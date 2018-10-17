c832ea3c58f98a8779ce8be2669179508a1b72b1
wkd <tomas@dekciw.lt>
2018-10-17 19:23:14 +0300

    show description everywhere

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -5,36 +5,33 @@
 	<?php endif; ?>
 
 	<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-		
-	<?php
-	// Only show description on front page.
-	$description = ( is_home() || is_front_page() ) ? get_bloginfo( 'description', 'display' ) : null;
-	?>
-	<?php if( $description || ( is_customize_preview() && ! is_null( $description ) ) ) : ?>
+
+	<?php $description = get_bloginfo( 'description', 'display' );
+		if( $description || is_customize_preview() ) : ?>
 		<p class="site-description">
 			<?php echo $description; ?>
 		</p>
 	<?php endif; ?>
 	<?php if ( has_nav_menu( 'menu-1' ) ) : ?>
 		<nav id="site-navigation" class="main-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Top Menu', 'twentynineteen' ); ?>">
-			<?php 
+			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'menu-1',
 				'menu_class'     => 'main-menu',
-			) ); 
+			) );
 			?>
 		</nav><!-- #site-navigation -->
 	<?php endif; ?>
 	<?php if ( has_nav_menu( 'social' ) ) : ?>
 		<nav class="social-navigation" role="navigation" aria-label="<?php esc_attr_e( 'Footer Social Links Menu', 'twentynineteen' ); ?>">
-			<?php 
+			<?php
 			wp_nav_menu( array(
 				'theme_location' => 'social',
 				'menu_class'     => 'social-links-menu',
 				'link_before'    => '<span class="screen-reader-text">',
 				'link_after'     => '</span>' . twentynineteen_get_icon_svg( 'link' ),
 				'depth'          => 1,
-			) ); 
+			) );
 			?>
 		</nav><!-- .social-navigation -->
 	<?php endif; ?>
