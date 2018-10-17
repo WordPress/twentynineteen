c578f801ce7f9738bf2fd922ed6e62ede14bde63
Jason Yingling <yingling017@gmail.com>
2018-10-16 22:04:02 -0500

    pointing get_template_part to correct path for content-none.php

--- archive.php
+++ archive.php
@@ -42,7 +42,7 @@ get_header(); ?>
 
 			// If no content, include the "No posts found" template.
 		else :
-			get_template_part( 'template-parts/content', 'none' );
+			get_template_part( 'template-parts/content/content', 'none' );
 
 		endif;
 		?>
