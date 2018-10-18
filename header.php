8a69900e685d8976b4381f2294c64af4f87df3a4
Allan Cole <allan.cole@automattic.com>
2018-10-18 09:27:45 -0400

    Removes the featured header image from index, archives and search results, see #38, Fixes #37

--- header.php
+++ header.php
@@ -23,7 +23,7 @@
 <div id="page" class="site">
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentynineteen' ); ?></a>
 
-	<?php if ( is_home() || is_front_page() ) { ?>
+	<?php if ( is_home() ) { ?>
 
 		<header id="masthead" class="site-header">
 			<?php get_template_part( 'template-parts/header/site', 'branding' ) ?>
@@ -31,12 +31,12 @@
 
 	<?php } else { ?>
 
-		<header id="masthead" class="<?php echo ! is_archive() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header' ?>">
+		<header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header' ?>">
 			<div class="site-branding-container">
 				<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
 			</div><!-- .layout-wrap -->
 
-			<?php if ( ! is_archive() && twentynineteen_can_show_post_thumbnail() ) : ?>
+			<?php if ( is_singular() && twentynineteen_can_show_post_thumbnail() ) : ?>
 				<div class="hentry">
 					<?php the_post(); ?>
 					<div class="entry-header">
