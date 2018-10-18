d64b1624ddb5fddd0639f5a513c2fc5fce719bd5
Brianna Privett <briannaorg@gmail.com>
2018-10-18 14:40:19 -0500

    Remove estimated read time
    Per discussion on issue #139, removing the estimated read time since
    it’s in plugin territory. Also removed the function from
    inc/template-tags.php

--- header.php
+++ header.php
@@ -39,7 +39,6 @@
 						<?php if ( ! is_page() ) : ?>
 						<div class="<?php echo ( ! empty( $discussion ) && count( $discussion->authors ) > 0 ) ? 'entry-meta has-discussion' : 'entry-meta'; ?>">
 							<?php twentynineteen_posted_by(); ?>
-							<?php twentynineteen_estimated_read_time(); ?>
 							<span class="comment-count">
 								<?php if ( ! empty( $discussion ) ) twentynineteen_discussion_avatars_list( $discussion->authors ); ?>
 								<?php twentynineteen_comment_count(); ?>
