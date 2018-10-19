eeaf7fb49477fe76fe434376c4f2529a53685c20
Allan Cole <allan.cole@automattic.com>
2018-10-19 12:19:40 -0400

    Adds dropdown menu support for desktop view, includes accessibility styles for focus and tab-key-browsing in sub-menus

--- inc/template-functions.php
+++ inc/template-functions.php
@@ -190,3 +190,24 @@ function twentynineteen_get_discussion_data() {
 	);
 	return $discussion;
 }
+
+/**
+ * WCAG 2.0 Attributes for Dropdown Menus
+ *
+ * Adjustments to menu attributes tot support WCAG 2.0 recommendations
+ * for flyout and dropdown menus.
+ *
+ * @ref https://www.w3.org/WAI/tutorials/menus/flyout/
+ */
+function twentynineteen_nav_menu_link_attributes( $atts, $item, $args, $depth ) {
+
+    // Add [aria-haspopup] and [aria-expanded] to menu items that have children
+    $item_has_children = in_array( 'menu-item-has-children', $item->classes );
+    if ( $item_has_children ) {
+        $atts['aria-haspopup'] = "true";
+        $atts['aria-expanded'] = "false";
+    }
+
+    return $atts;
+}
+add_filter( 'nav_menu_link_attributes', 'twentynineteen_nav_menu_link_attributes', 10, 4 );
