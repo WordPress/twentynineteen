ceb7d9bbf6204f05aa3ec2e96f8f8c9b90166011
Danny Cooper <info@dannycooper.org>
2018-10-19 10:35:50 +0100

    Update content-single.php
    fix whitespace

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -17,22 +17,22 @@
 		if ( ! is_page() ) {
 			$discussion = twentynineteen_can_show_post_thumbnail() ? twentynineteen_get_discussion_data() : null;
 		}
-		
+
 		the_title( '<h1 class="entry-title">', '</h1>' );
-		
-		if ( ! is_page() ) : 
+
+		if ( ! is_page() ) :
 			?>
 			<div class="<?php echo ( ! empty( $discussion ) && count( $discussion->authors ) > 0 ) ? 'entry-meta has-discussion' : 'entry-meta'; ?>">
-				<?php 
-				twentynineteen_posted_by(); 
-				twentynineteen_estimated_read_time(); 
+				<?php
+				twentynineteen_posted_by();
+				twentynineteen_estimated_read_time();
 				?>
 				<span class="comment-count">
 					<?php
 					if ( ! empty( $discussion ) ) {
 						twentynineteen_discussion_avatars_list( $discussion->authors );
 					}
-					twentynineteen_comment_count(); 
+					twentynineteen_comment_count();
 					?>
 				</span>
 				<?php
