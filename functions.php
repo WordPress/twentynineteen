7a928f5eaad52cec61315a47a87cafdc55f4a8ab
Ashar Irfan <mrasharirfan@gmail.com>
2018-10-20 02:44:09 +0500

    ❌ REMOVE: Jetpack Support

--- functions.php
+++ functions.php
@@ -172,10 +172,3 @@ require get_template_directory() . '/inc/template-tags.php';
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
-
-/**
- * Load Jetpack compatibility file.
- */
-if ( defined( 'JETPACK__VERSION' ) ) {
-	require get_template_directory() . '/inc/jetpack.php';
-}
