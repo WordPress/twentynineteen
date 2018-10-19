c8a6f2adde12684c4b1949266aea31c613a11561
Kjell Reigstad <kjell@kjellr.com>
2018-10-19 19:54:36 -0400

    Replace site title h1 tag with p when not on homepage.
    For better SEO, and for preventing the appearance of more than one h1 on the page.

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -12,8 +12,12 @@
 		<div class="site-logo"><?php the_custom_logo(); ?></div>
 	<?php endif; ?>
 
-	<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-
+	<?php if ( is_front_page() && is_home() ) : ?>
+		<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+	<?php else : ?>
+		<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+	<?php endif; ?>
+	
 	<?php
 		$description = get_bloginfo( 'description', 'display' );
 		if ( $description || is_customize_preview() ) :
