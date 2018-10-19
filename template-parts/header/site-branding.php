53a69e0a608c8ebb079cbe104c8e8f3fc01942ff
wkd <tomas@dekciw.lt>
2018-10-19 15:45:57 +0300

    fix travis-ci

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -14,8 +14,10 @@
 
 	<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
 
-	<?php $description = get_bloginfo( 'description', 'display' );
-		if( $description || is_customize_preview() ) : ?>
+	<?php
+		$description = get_bloginfo( 'description', 'display' );
+		if ( $description || is_customize_preview() ) :
+	?>
 			<p class="site-description">
 				<?php echo $description; ?>
 			</p>
