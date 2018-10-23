9d6515e88bb03b763d2696166235922420b1151d
Weston Ruter <westonruter@google.com>
2018-10-23 12:08:22 -0700

    Minify skip-link-focus-fix.js to inline in twentynineteen_skip_link_focus_fix()

--- functions.php
+++ functions.php
@@ -155,7 +155,8 @@ add_action( 'wp_enqueue_scripts', 'twentynineteen_scripts' );
  */
 function twentynineteen_skip_link_focus_fix() {
 	echo '<script>';
-	echo file_get_contents( get_template_directory() . '/js/skip-link-focus-fix.js' );
+	// The following is minified via `terser --compress --mangle -- js/skip-link-focus-fix.js`.
+	echo '/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);';
 	echo '</script>';
 }
 add_action( 'wp_print_footer_scripts', 'twentynineteen_skip_link_focus_fix' );
