0df547abbf316602e67c657a23ceecfc426749c5
Josh Feck <josh@nxnweb.com>
2018-10-24 11:33:14 -0400

    Remove html from Edit strings

--- template-parts/content/content-page.php
+++ template-parts/content/content-page.php
@@ -36,16 +36,9 @@
 			<?php
 			edit_post_link(
 				sprintf(
-					wp_kses(
-						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'twentynineteen' ),
-						array(
-							'span' => array(
-								'class' => array(),
-							),
-						)
-					),
-					get_the_title()
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					esc_html__( 'Edit %s', 'twentynineteen' ),
+					'<span class="screen-reader-text">' . get_the_title() . '</span>'
 				),
 				'<span class="edit-link">',
 				'</span>'
