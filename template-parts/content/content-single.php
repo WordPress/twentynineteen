9afec68c38e5169e152139999e9128e3291a8c31
Josh Feck <josh@nxnweb.com>
2018-10-24 11:32:07 -0400

    Remove html from Edit and Continue reading strings

--- template-parts/content/content-single.php
+++ template-parts/content/content-single.php
@@ -32,16 +32,9 @@
 			// Edit post link.
 				edit_post_link(
 					sprintf(
-						wp_kses(
-							/* translators: %s: Name of current post. Only visible to screen readers. */
-							__( 'Edit <span class="screen-reader-text">%s</span>', 'twentynineteen' ),
-							array(
-								'span' => array(
-									'class' => array(),
-								),
-							)
-						),
-						get_the_title()
+						/* translators: %s: Name of current post. Only visible to screen readers. */
+						esc_html__( 'Edit %s', 'twentynineteen' ),
+						 '<span class="screen-reader-text"> ' . get_the_title() . '</span>'
 					),
 					'<span class="edit-link">' . twentynineteen_get_icon_svg( 'edit', 16 ),
 					'</span>'
@@ -56,16 +49,9 @@
 		<?php
 		the_content(
 			sprintf(
-				wp_kses(
-					/* translators: %s: Name of current post. Only visible to screen readers */
-					__( 'Continue reading<span class="screen-reader-text"> "%s"</span>', 'twentynineteen' ),
-					array(
-						'span' => array(
-							'class' => array(),
-						),
-					)
-				),
-				get_the_title()
+				/* translators: %s: Name of current post. Only visible to screen readers */
+				esc_html__( 'Continue reading%s', 'twentynineteen' ),
+				'<span class="screen-reader-text"> "' . get_the_title() . '"</span>'
 			)
 		);
 
