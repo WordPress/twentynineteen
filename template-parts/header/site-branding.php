3af21e8620d8964ac3c9eae3ae32d6664e33f84e
Niels Lange <info@nielslange.de>
2018-10-25 20:25:32 +0700

    Fix Travic CI violations

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -13,13 +13,13 @@
 		<div class="site-logo"><?php the_custom_logo(); ?></div>
 	<?php endif; ?>
 
-    <?php if ( ! empty( get_bloginfo( 'name' ) ) ) : ?>
-        <?php if ( is_front_page() && is_home() ) : ?>
-            <h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-        <?php else : ?>
-            <p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-        <?php endif; ?>
-    <?php endif; ?>
+	<?php if ( ! empty( get_bloginfo( 'name' ) ) ) : ?>
+		<?php if ( is_front_page() && is_home() ) : ?>
+			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<?php else : ?>
+			<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+		<?php endif; ?>
+	<?php endif; ?>
 
 	<?php
 	$description = get_bloginfo( 'description', 'display' );
