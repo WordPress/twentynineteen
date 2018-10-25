6f78c34f32c3c303ee83a0929edb2f699ad35812
Allan Cole <allan.cole@automattic.com>
2018-10-24 23:43:10 -0400

    Improve featured image class structure in site-header

--- header.php
+++ header.php
@@ -24,12 +24,13 @@
 	<a class="skip-link screen-reader-text" href="#content"><?php esc_html_e( 'Skip to content', 'twentynineteen' ); ?></a>
 
 		<header id="masthead" class="<?php echo is_singular() && twentynineteen_can_show_post_thumbnail() ? 'site-header featured-image' : 'site-header'; ?>">
+
 			<div class="site-branding-container">
 				<?php get_template_part( 'template-parts/header/site', 'branding' ); ?>
 			</div><!-- .layout-wrap -->
 
 			<?php if ( is_singular() && twentynineteen_can_show_post_thumbnail() ) : ?>
-				<div class="hentry">
+				<div class="site-featured-image">
 					<?php the_post(); ?>
 					<div class="entry-header">
 						<?php if ( ! is_page() ) : ?>
