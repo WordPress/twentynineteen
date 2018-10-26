257f1ce8a4504a5964e50ecf8e5973d4ff819416
Niels Lange <info@nielslange.de>
2018-10-26 15:57:13 +0700

    Add Greek language support

--- functions.php
+++ functions.php
@@ -145,6 +145,12 @@ function twentynineteen_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+	switch ( get_locale() ) {
+		case 'el':
+			wp_enqueue_style( 'twentynineteen-greek', get_template_directory_uri() . '/language/greek.css' );
+			break;
+	}
 }
 add_action( 'wp_enqueue_scripts', 'twentynineteen_scripts' );
 
