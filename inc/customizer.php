be361cdf669413997ce96e54b85fce6d2fa8ddad
Elio Rivero <eliorivero@gmail.com>
2018-10-26 14:53:15 -0300

    Customizer: add extra label after description for option to set image filter

--- inc/customizer.php
+++ inc/customizer.php
@@ -49,7 +49,7 @@ function twentynineteen_customize_register( $wp_customize ) {
 			'label'       => __( 'Featured Image Color Filter', 'twentynineteen' ),
 			'section'     => 'colors',
 			'type'        => 'radio',
-			'description' => __( "By default, Twenty Nineteen adds a duotone-like effect to featured images using your site's primary color. This effect can be turned off. When the color filter is disabled, a standard black overlay appears on individual post pages to preserve readability of the text on top of the featured image.", 'twentynineteen' ),
+			'description' => __( "By default, Twenty Nineteen adds a duotone-like effect to featured images using your site's primary color. This effect can be turned off. When the color filter is disabled, a standard black overlay appears on individual post pages to preserve readability of the text on top of the featured image.", 'twentynineteen' ) . '<br/><strong style="font-style: normal; display: block; margin-top: 8px;">' . __( 'Featured images should:', 'twentynineteen' ) . '</strong>',
 			'choices'     => array(
 				'active'   => __( 'Have a color filter applied.', 'twentynineteen' ),
 				'inactive' => __( 'Not have a color filter applied.', 'twentynineteen' ),
