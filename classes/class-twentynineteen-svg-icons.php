45d45151f072e6295d2ca480ec7a184312c8e838
Weston Ruter <westonruter@google.com>
2018-10-25 22:19:31 -0700

    Add keyboard_arrow_left material-design icon

--- classes/class-twentynineteen-svg-icons.php
+++ classes/class-twentynineteen-svg-icons.php
@@ -154,6 +154,12 @@ class TwentyNineteen_SVG_Icons {
 	<path fill="none" d="M0 24V0h24v24H0z"/>
 </svg>',
 
+		'keyboard_arrow_left'   => /* material-design – keyboard_arrow_left */ '
+<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+	<path d="M14 7l-5 5 5 5V7z"/>
+	<path fill="none" d="M24 0v24H0V0h24z"/>
+</svg>',
+
 	);
 
 	/**
