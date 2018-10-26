6cdd6da4870198240eb2001871448cfdc053c8bc
Niels Lange <info@nielslange.de>
2018-10-26 18:23:48 +0700

    Add Chinese language support

--- functions.php
+++ functions.php
@@ -145,6 +145,14 @@ function twentynineteen_scripts() {
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
+
+	switch ( get_locale() ) {
+		case 'zh_HK':
+		case 'zh_TW':
+		case 'zh_CN':
+			wp_enqueue_style( 'twentynineteen-chinese', get_template_directory_uri() . '/language/chinese.css' );
+			break;
+	}
 }
 add_action( 'wp_enqueue_scripts', 'twentynineteen_scripts' );
 
