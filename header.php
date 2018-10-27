746d8ae495db7d4309fb0701e0acc91845e9d32e
Allan Cole <allan.cole@automattic.com>
2018-10-27 11:12:29 -0400

    Fix post-discussion avatar list display

--- header.php
+++ header.php
@@ -32,7 +32,8 @@
 			<?php if ( is_singular() && twentynineteen_can_show_post_thumbnail() ) : ?>
 				<div class="site-featured-image">
 					<?php the_post(); ?>
-					<div class="entry-header">
+					<?php $discussion = ! is_page() && twentynineteen_can_show_post_thumbnail() ? twentynineteen_get_discussion_data() : null; ?>
+					<div class="<?php echo ( ! empty( $discussion ) && count( $discussion->authors ) > 0 ) ? 'entry-header has-discussion' : 'entry-header'; ?>">
 						<?php get_template_part( 'template-parts/header/entry', 'header' ); ?>
 					</div><!-- .entry-header -->
 					<?php rewind_posts(); ?>
--- template-parts/header/entry-header.php
+++ template-parts/header/entry-header.php
@@ -7,14 +7,12 @@
  * @since 1.0.0
  */
 
-if ( ! is_page() ) : ?>
-<?php $discussion = twentynineteen_can_show_post_thumbnail() ? twentynineteen_get_discussion_data() : null; ?>
-<?php endif; ?>
+$discussion = ! is_page() && twentynineteen_can_show_post_thumbnail() ? twentynineteen_get_discussion_data() : null; ?>
 
 <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
 
 <?php if ( ! is_page() ) : ?>
-<div class="<?php echo ( ! empty( $discussion ) && count( $discussion->authors ) > 0 ) ? 'entry-meta has-discussion' : 'entry-meta'; ?>">
+<div class="entry-meta">
 	<?php twentynineteen_posted_by(); ?>
 	<?php twentynineteen_posted_on(); ?>
 	<span class="comment-count">
