e355f5118ee0518615543fda3176e86886728bea
Kjell Reigstad <kjell@kjellr.com>
2018-10-29 09:54:45 -0400

    Fix linting errors, recompile RTL stylesheet.

--- functions.php
+++ functions.php
@@ -118,15 +118,17 @@ add_action( 'after_setup_theme', 'twentynineteen_setup' );
  */
 function twentynineteen_widgets_init() {
 
-	register_sidebar( array(
-		'name'          => __( 'Footer 1', 'twentynineteen' ),
-		'id'            => 'sidebar-1',
-		'description'   => __( 'Add widgets here to appear in your footer.', 'twentynineteen' ),
-		'before_widget' => '<section id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</section>',
-		'before_title'  => '<h2 class="widget-title">',
-		'after_title'   => '</h2>',
-	) );
+	register_sidebar( 
+		array(
+			'name'          => __( 'Footer 1', 'twentynineteen' ),
+			'id'            => 'sidebar-1',
+			'description'   => __( 'Add widgets here to appear in your footer.', 'twentynineteen' ),
+			'before_widget' => '<section id="%1$s" class="widget %2$s">',
+			'after_widget'  => '</section>',
+			'before_title'  => '<h2 class="widget-title">',
+			'after_title'   => '</h2>',
+		) 
+	);
 
 }
 add_action( 'widgets_init', 'twentynineteen_widgets_init' );
