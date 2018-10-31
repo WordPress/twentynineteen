964373e8349fa877b30cf1c5085bfbd5cf0249f6
Csaba <Csaba@csabas-mbp.home>
2018-10-31 15:33:47 +0100

    fix failed checks
    trying to fix failed checks Travis

--- inc/back-compat.php
+++ inc/back-compat.php
@@ -48,9 +48,11 @@ function twentynineteen_upgrade_notice() {
  * @global string $wp_version WordPress version.
  */
 function twentynineteen_customize() {
-	wp_die( sprintf( __( 'Twenty Nineteen requires at least WordPress version 4.7. You are running version %s. Please upgrade and try again.', 'twentynineteen' ), $GLOBALS['wp_version'] ), '', array(
-		'back_link' => true,
-	) );
+	wp_die(
+		sprintf( __( 'Twenty Nineteen requires at least WordPress version 4.7. You are running version %s. Please upgrade and try again.', 'twentynineteen' ), $GLOBALS['wp_version'] ),
+		'',
+		array( 'back_link' => true ),
+	);
 }
 add_action( 'load-customize.php', 'twentynineteen_customize' );
 
