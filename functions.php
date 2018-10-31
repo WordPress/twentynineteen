8044854490fa83bef5bfd97dc9cbb94d8ec7f139
Csaba <Csaba@csabas-mbp.home>
2018-10-31 15:21:38 +0100

    add back compat file
    add back compat file from Twenty Seventeen

--- functions.php
+++ functions.php
@@ -12,7 +12,7 @@
 /**
  * Twenty Nineteen only works in WordPress 4.7 or later.
  */
-if ( version_compare( $GLOBALS['wp_version'], '4.7-alpha', '<' ) ) {
+if ( version_compare( $GLOBALS['wp_version'], '4.7', '<' ) ) {
 	require get_template_directory() . '/inc/back-compat.php';
 	return;
 }
