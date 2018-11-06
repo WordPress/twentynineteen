94c0655e386221aaab9a0d6768da2df833b46339
Weston Ruter <westonruter@google.com>
2018-11-06 08:38:52 -0800

    Output script without echoes

--- functions.php
+++ functions.php
@@ -191,10 +191,12 @@ add_action( 'wp_enqueue_scripts', 'twentynineteen_scripts' );
  * @link https://git.io/vWdr2
  */
 function twentynineteen_skip_link_focus_fix() {
-	echo '<script>';
 	// The following is minified via `terser --compress --mangle -- js/skip-link-focus-fix.js`.
-	echo '/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);';
-	echo '</script>';
+	?>
+	<script>
+	/(trident|msie)/i.test(navigator.userAgent)&&document.getElementById&&window.addEventListener&&window.addEventListener("hashchange",function(){var t,e=location.hash.substring(1);/^[A-z0-9_-]+$/.test(e)&&(t=document.getElementById(e))&&(/^(?:a|select|input|button|textarea)$/i.test(t.tagName)||(t.tabIndex=-1),t.focus())},!1);
+	</script>
+	<?php
 }
 add_action( 'wp_print_footer_scripts', 'twentynineteen_skip_link_focus_fix' );
 
