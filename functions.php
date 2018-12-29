67d6e4edd85a284d12b5bf87b246f298bba5d211
Julien Maury <6878027+jmau111@users.noreply.github.com>
2018-12-29 19:27:14 +0100

    bump up version for enqueue script
    as touch-keyboard-navigation.js has changed, bump up version for enqueue and cache busting

--- functions.php
+++ functions.php
@@ -223,7 +223,7 @@ function twentynineteen_scripts() {
 
 	if ( has_nav_menu( 'menu-1' ) ) {
 		wp_enqueue_script( 'twentynineteen-priority-menu', get_theme_file_uri( '/js/priority-menu.js' ), array(), '1.1', true );
-		wp_enqueue_script( 'twentynineteen-touch-navigation', get_theme_file_uri( '/js/touch-keyboard-navigation.js' ), array(), '1.0', true );
+		wp_enqueue_script( 'twentynineteen-c', get_theme_file_uri( '/js/touch-keyboard-navigation.js' ), array(), '1.1', true );
 	}
 
 	wp_enqueue_style( 'twentynineteen-print-style', get_template_directory_uri() . '/print.css', array(), wp_get_theme()->get( 'Version' ), 'print' );
